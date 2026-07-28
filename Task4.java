9c3353bcefe2d332ecbc48add0c66203c9ed43fe
Pratik Marathe <86453383+PratikVMarathe@users.noreply.github.com>
2026-07-28 14:28:05 +0530

    Add imports for ArrayList and List in Task4.java

--- Task4.java
+++ Task4.java
@@ -3,6 +3,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task4 {
 
     private DataSource dataSource;
